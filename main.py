955c85b67cfcdf77048aa8671e8bdb2ea193709a
Dkz17k <dhuansouza12@gmail.com>
2025-08-22 16:35:08 -0300

    Atualizando main.py com melhorias

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
+import json
 import os
-
-usuarios = [{'nome':'Dhuan','categoria':'cliente','ativado':True},{'nome':'Ilenir','categoria':'cliente','ativado':False},{'nome':'Ivonir','categoria':'cliente','ativado':True},{'nome':'Arthur','categoria':'cliente','ativado':False}]
 
 def nome_do_programa():
     os.system('cls')
@@ -24,8 +22,14 @@ def voltar_ao_menu_principal():
     main()
 
 def login():
-    str(input('\nPara fazer seu login, digite seu Nome de Usuário: '))
-    print('Logado com sucesso!')
+    token = input('\nPara fazer seu login, digite seu Nome de Usuário: ')
+    senha = input('\nAgora digite sua senha para fazer login: ')
+    for usuario in usuarios:
+        if token.lower() == usuario['nome'].lower() and senha.lower() == usuario['senha'].lower():
+            print('Login feito com sucesso.')
+            voltar_ao_menu_principal()
+    print('Usuário ou senha incorreto, tente novamente.')
+    voltar_ao_menu_principal()
 
 def ativar_usuario_ativado():
     ativar_cadastro = input(str('Digite o nome do cliente que deseja ativar: '))
@@ -35,20 +39,39 @@ def ativar_usuario_ativado():
             usuarioencontrado = True
             cliente['ativado'] = True
             print('Usuário ativado com sucesso! Fique a vontade para usar sua conta bancária.')
+            salvar_usuarios()
     if not usuarioencontrado:
         print('Usuário não encontrado')
     voltar_ao_menu_principal()
-    main()
+
+def carregar_users():
+    if os.path.exists('usuarios.json'):
+        with open('usuarios.json', 'r', encoding='utf-8',) as users:
+            return json.load(users)
+    else:
+        return []
+
+usuarios = carregar_users()
+
+def salvar_usuarios():
+    with open('usuarios.json', 'w', encoding='utf-8') as users:
+        json.dump(usuarios, users, ensure_ascii=False, indent=4)
 
 def cadastrar_usuario():
     os.system('cls')
     usuario_ativado = input('Coloque o Nome de Usuário que você deseja cadastrar: ')
+    usuario_senha = input('Insira uma senha para o Usuário que você deseja cadastrar: ')
     usuario_categoria = input('Insira a categoria do Usuário que você deseja cadastrar: ')
     usuario_esta_ativo = False
     print('Usuário ativado com sucesso!')
-    usuarios.append({'nome': usuario_ativado,'categoria': usuario_categoria,'ativado': usuario_esta_ativo})
+    usuarios.append({
+        'nome': usuario_ativado,
+        'categoria': usuario_categoria,
+        'ativado': usuario_esta_ativo,
+        'senha': usuario_senha
+        })
+    salvar_usuarios()
     voltar_ao_menu_principal()
-    main()
 
 def listar_usuario():
     os.system('cls')
@@ -59,48 +82,25 @@ def listar_usuario():
         esta_ativado = 'Conta Ativada!' if pessoa['ativado'] else 'A conta ainda não está ativada.'
         print(f'|{nome_cliente}\n|{categoria_client}\n|Estado da conta: {esta_ativado}\n')
     voltar_ao_menu_principal()
-    main()
-    '''
-    Função que exibe uma lista de todos os usuários com base no seu cadastro: Nome; Categoria; 
-    '''
-        
+    
 def depositar():
     str(input('Quanto você deseja depositar? '))
     print('Depositado com sucesso!')
-    '''
-    Função que pergunta ao usuário a quantia de dinheiro que ele deseja depositar com base no valor inserido pelo usuário. ( sem validações por ser um projeto simples )
-    '''
 
 def retirar():
     str(input('Quanto você deseja retirar? '))
     print('Dinheiro retirado com sucesso!')
-    '''
-    Função que pergunta ao usuário a quantia de dinheiro que ele deseja retirar com base no valor inserido pelo usuário. ( sem validações por ser um projeto simples )
-    '''
 
 def denunciar():
     print('Para fazer uma Denúncia, preencha o formulário a seguir: https://dk/banco/report.com')
-    '''
-    Função que exibe uma mensagem indicando um caminho para o usuário registrar uma denuncia.
-    '''
-
-def sair():
-    os.system('cls')
-    print('Saindo do app...\n')
-    '''
-    Função que fecha a aplicação e encerra sua execução.
-    '''
 
 def opcao_invalida():
     print('\nOpção invalida!')
     voltar_ao_menu_principal()
-    main()
-    '''
-    Função que trata um erro do usuário ao escolher uma função inválida.
-    '''
 
 def escolher_opcao():
     try:
+        global sair
         desejo = int(input('Oque deseja fazer? '))
         if desejo == 1:
             login()
@@ -115,24 +115,24 @@ def escolher_opcao():
         elif desejo == 7:
             denunciar()
         elif desejo == 8:
-            sair()
+            sair = True
         elif desejo == 4:
             ativar_usuario_ativado()
         else:
             opcao_invalida()
     except:
         opcao_invalida()
-    '''
-    Função que pergunta ao usuário o que ele deseja fazer na plataforma e executando uma determinada ação conforme seu desejo.
-    ''' 
+
+sair = False
 
 def main():
-    nome_do_programa()
-    funcoes_do_programa()
-    escolher_opcao()
-    '''
-    Função principal, a raíz do projeto que executa organizadamente cada bloco de codigo.
-    '''
+    while True:
+        nome_do_programa()
+        funcoes_do_programa()
+        escolher_opcao()
+        if sair:
+            print('\n' * 100)
+            break
 
 if __name__ == '__main__':
     main()
